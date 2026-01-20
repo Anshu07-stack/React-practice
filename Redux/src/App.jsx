3fba2805446ea26165b2fad9b1ab95f5559894fb
Anshu Kumar <venomanshu879177@gmail.com>
2026-01-20 08:01:03 +0530

    know about what is reducer,actioN and state in redux

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {useSelector,useDispatch} from 'react-redux'
+import { addToProduct } from './reduxx/action/productAction'
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch()
+  const state = useSelector((state)=>{
+    return state
+  })
+  console.log(state)
 
   return (
    <>
       <div className='bg-red-500'>hello</div>
+      <button onClick={()=>dispatch(addToProduct())}>click</button>
    </>
   )
 }
---
+++ Redux/src/reduxx/action/productAction.js
@@ -0,0 +1,6 @@
+export const addToProduct = ()=>{
+    return {
+        type:'ADD_TO_PRODUCT',
+        payload:'' 
+    }
+}
